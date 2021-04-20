ac98aeabfccb23993b8c85e1175f1a6441b6573e
DESKTOP-TAUOQ1K\joseb <josebueno@wposs.com>
2021-04-20 10:03:02 -0500

    Se crea modelo para consumo del API y se agregan librerías de Retrofit y RecyclerView

--- app/src/main/java/com/jomibusa/themoviedb/modelo/Movie.java
+++ app/src/main/java/com/jomibusa/themoviedb/modelo/Movie.java
@@ -1,4 +1,49 @@
 package com.jomibusa.themoviedb.modelo;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class Movie {
+
+    @SerializedName("page")
+    private Integer page;
+    @SerializedName("results")
+    private List<Result> results;
+    @SerializedName("total_results")
+    private Integer totalResults;
+    @SerializedName("total_pages")
+    private Integer totalPages;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Integer getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(Integer totalPages) {
+        this.totalPages = totalPages;
+    }
 }
